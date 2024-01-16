d2609cf685871e054675e6999b82d61b42463736
morysmh <morteza.5761@gmail.com>
2024-01-17 01:13:41 +0330

    git rm build folder

--- Client/build/CMakeFiles/faridgrinder_pio_rotary_encoder_pio_h.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for faridgrinder_pio_rotary_encoder_pio_h.
-# This may be replaced when dependencies are built.
--- Client/build/CMakeFiles/faridgrinder_quadrature_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/faridgrinder_quadrature_pio_h"
-  "quadrature.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/faridgrinder_quadrature_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
